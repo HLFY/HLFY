b07163b12daba84dd5db9bac39c392600264f7d4
Łukasz Kuczborski <lukasz.kuczborski@me.com>
2014-11-15 17:05:01 +0100

    Added sample data

--- hlfy/hlfy/ViewController.swift
+++ hlfy/hlfy/ViewController.swift
@@ -11,6 +11,8 @@ import HealthKit
 
 class ViewController: UIViewController {
     
+    var healthStore: HKHealthStore = HKHealthStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,31 +25,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // Returns the types of data that Fit wishes to read from HealthKit.
+    // Returns the types of data that HLFY wishes to read from HealthKit.
     func dataTypesToRead() -> NSSet {
-        let dietaryCalorieEnergyType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDietaryEnergyConsumed);
-        let activeEnergyBurnType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned);
-        let heightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight);
-        let weightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass);
-        let birthdayType: HKObjectType = HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth);
-        let biologicalSexType: HKObjectType = HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex);
+        let distanceWalkingRunningType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        let stepsCountType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        let dietaryEnergyConsumedType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDietaryEnergyConsumed)
+        let heightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
+        let weightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+        let birthdayType: HKObjectType = HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth)
+        let biologicalSexType: HKObjectType = HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex)
 
-        return NSSet(objects: dietaryCalorieEnergyType, activeEnergyBurnType, heightType, weightType, birthdayType, biologicalSexType);
+        return NSSet(objects: distanceWalkingRunningType, stepsCountType, dietaryEnergyConsumedType, heightType, weightType, birthdayType, biologicalSexType)
+    }
+    
+    // Returns the types of data that HLFY wishes to write to HealthKit.
+    func dataTypesToWrite() -> NSSet {
+        let distanceWalkingRunningType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        let stepsCountType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        let dietaryEnergyConsumedType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDietaryEnergyConsumed)
+        let heightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
+        let weightType: HKObjectType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+        
+        return NSSet(objects: distanceWalkingRunningType, stepsCountType, dietaryEnergyConsumedType, heightType, weightType)
     }
     
     func getHealthKitData() {
-        var healthStore: HKHealthStore = HKHealthStore()
         if (HKHealthStore.isHealthDataAvailable()) {
-            
-            healthStore.requestAuthorizationToShareTypes(nil, readTypes: self.dataTypesToRead(), completion:
-                { (bool, error) -> Void in
-                    var err: NSError?
-                    println(healthStore.dateOfBirthWithError(&err))
-                })
+            healthStore.requestAuthorizationToShareTypes(dataTypesToWrite(), readTypes: dataTypesToRead(), completion: { (success, error) -> Void in
+                if(success && error == nil) {
+                    self.mockHealtKitData();
+                }
+            })
         }
-
     }
-
-
+    
+    func mockHealtKitData() {
+        
+        let date : NSDate = NSDate();
+        
+        let distanceWalkingRunningType: HKQuantityType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: 10000)
+        let distanceSample: HKQuantitySample = HKQuantitySample(type: distanceWalkingRunningType, quantity: distanceQuantity, startDate: date.dateByAddingTimeInterval(-10000), endDate: date)
+        
+        let stepsCountType: HKQuantityType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        let stepsQuantity = HKQuantity(unit: HKUnit.countUnit(), doubleValue: 9000)
+        let stepsSample: HKQuantitySample = HKQuantitySample(type: stepsCountType, quantity: stepsQuantity, startDate: date.dateByAddingTimeInterval(-10000), endDate: date)
+        
+        healthStore.saveObjects([distanceSample, stepsSample], withCompletion: { (success, error) -> Void in
+            if(success) {
+                println("Moar Data!")
+            } else {
+                println("ZONK!")
+            }
+        })
+    }
 }
 
