1be25e98e50dd099bd2288a6d25c6f535687d9d4
krzysztofsiejkowski <ksj@touk.pl>
2014-11-15 22:55:41 +0100

    communicate generation structure kind of stable

--- hlfy/hlfy/CommunicateGenerator.swift
+++ hlfy/hlfy/CommunicateGenerator.swift
@@ -12,12 +12,33 @@ import Darwin
 struct CommunicateGenerator {
     
     func generateFromData(data: [CauseEffectSuggestion]) -> String {
-        let randomIndex = Int(arc4random_uniform(UInt32(data.count) - UInt32(0)) + UInt32(data.count))
-        return descriptionForSuggestion(data[randomIndex])
+        if data.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(data.count) - UInt32(0)) + UInt32(data.count))
+            return descriptionForData(data[randomIndex])
+        } else {
+            return NSLocalizedString("widgetDefaultCommunicate", comment: "")
+        }
     }
     
-    func descriptionForSuggestion(suggestion: CauseEffectSuggestion) -> String {
-        return "cokolwiek"
+    func descriptionForData(suggestion: CauseEffectSuggestion) -> String {
+        var communicate = ""
+        var fullStop = true
+        (fullStop, communicate) = descriptionForCause(fullStop, suggestion.cause, communicate)
+        (fullStop, communicate) = descriptionForEffect(fullStop, suggestion.effect, communicate)
+        (fullStop, communicate) = descriptionForSuggestion(fullStop, suggestion.suggestion, communicate)
+        return communicate
+    }
+    
+    func descriptionForCause(fullStop: Bool, _ cause: DataInsight, _ communicate: String) -> (Bool, String) {
+        return (true, "Bla.")
+    }
+
+    func descriptionForEffect(fullStop: Bool, _ cause: DataInsight, _ communicate: String) -> (Bool, String) {
+        return (true, "Ble.")
+    }
+
+    func descriptionForSuggestion(fullStop: Bool, _ cause: DataInsight, _ communicate: String) -> (Bool, String) {
+         return (true, "Blu.")
     }
     
 }
--- hlfy/hlfy/CommunicateProcessor.swift
+++ hlfy/hlfy/CommunicateProcessor.swift
@@ -28,71 +28,13 @@ struct CommunicateProcessor {
     }
     
     func processDataInsights(dataInsights: [DataInsight]) -> [CauseEffectSuggestion] {
-        for insight in dataInsights {
-            switch insight {
-            case .Weight:
-                generateWeightForTrend(insight)
-            case .Step:
-                 generateStepForTrend(insight)
-            case .Sleep:
-                generateSleepForTrend(insight)
-            case .Distance:
-                generateDistanceForTrend(insight)
-            }
-        }
-        return [CauseEffectSuggestion(cause: dataInsights[0], effect: dataInsights[0], suggestion: dataInsights[0])]
+        var suggestions : [CauseEffectSuggestion] = []
+        suggestions += suggestionsForSleepDistanceHypothesis(dataInsights)
+        return suggestions
     }
     
-    func generateWeightForTrend(trend: DataInsight) -> String {
-        switch trend {
-        case .Weight(.Ascending, _):
-            return "chudniesz"
-        case .Weight(.Descending, _):
-            return "grubisz"
-        case .Weight(.Steady, _):
-            return "się trzymasz"
-        default:
-            return ""
-        }
-    }
-    
-    func generateStepForTrend(trend: DataInsight) -> String {
-        switch trend {
-        case .Step(.Ascending, _):
-            return "dajesz!"
-        case .Step(.Descending, _):
-            return "oj, pokrokuj trochę"
-        case .Step(.Steady, _):
-            return "trzymasz tempo"
-        default:
-            return ""
-        }
-    }
-    
-    func generateSleepForTrend(trend: DataInsight) -> String {
-        switch trend {
-        case .Sleep(.Ascending, _):
-            return "ty śpiochu!"
-        case .Sleep(.Descending, _):
-            return "weź się połóż"
-        case .Sleep(.Steady, _):
-            return "śpij spokojnie"
-        default:
-            return ""
-        }
-    }
-    
-    func generateDistanceForTrend(trend: DataInsight) -> String {
-        switch trend {
-        case .Distance(.Ascending, _):
-            return "coraz dalej"
-        case .Distance(.Descending, _):
-            return "coraz bliżej"
-        case .Distance(.Steady, _):
-            return "metoda żabich kroków"
-        default:
-            return ""
-        }
+    private func suggestionsForSleepDistanceHypothesis(dataInsights: [DataInsight]) -> [CauseEffectSuggestion] {
+        return []
     }
     
 }
